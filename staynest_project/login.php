5aeabfd54e83a3b0bb94716baa2c868bc6a11ef7
AlAlifspecialist <alalifsarker@gmail.com>
2026-05-05 13:01:04 +0200

    Add files via upload

---
+++ staynest_project/login.php
@@ -0,0 +1,84 @@
+<?php
+include("config/db.php");
+session_start();
+
+$error = "";
+
+if(isset($_POST['login'])){
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    $sql = "SELECT * FROM users WHERE Email='$email'";
+    $result = $conn->query($sql) or die($conn->error);
+
+    if($result->num_rows > 0){
+        $user = $result->fetch_assoc();
+
+        if(trim($user['PasswordHash']) == $password){
+            $_SESSION['UserID'] = $user['UserID'];
+            $_SESSION['Email'] = $user['Email'];
+
+            header("Location: dashboard.php");
+            exit();
+        } else {
+            $error = "Invalid password";
+        }
+    } else {
+        $error = "Email not found";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>StayNest Login</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+
+<nav>
+    <h2>StayNest</h2>
+    <ul>
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>SERVICE</li>
+        <li>CONTACT</li>
+        <li class="login-btn">LOGIN</li>
+    </ul>
+</nav>
+
+<div class="login-box">
+    <h1>LOGIN</h1>
+
+    <?php
+    if($error != ""){
+        echo "<p class='error'>$error</p>";
+    }
+    ?>
+
+    <form method="POST">
+        <div class="input-box">
+            <input type="email" name="email" placeholder="Email" required>
+        </div>
+
+        <div class="input-box">
+            <input type="password" name="password" placeholder="Password" required>
+        </div>
+
+        <div class="options">
+            <label><input type="checkbox"> Remember Me</label>
+            <a href="#">Forgot Password?</a>
+        </div>
+
+        <button type="submit" name="login">Login</button>
+
+        <p class="register-text">
+            Don’t have an Account?
+            <a href="register.php">Register</a>
+        </p>
+    </form>
+</div>
+
+</body>
+</html>
